0f55062b79abf9e5bee92e8e01296dd953b0eb37
Juin Chiu <juin.chiu@infinitechain.io>
2018-03-19 17:15:55 +0800

    Fix client.js

--- client.js
+++ client.js
@@ -1,63 +1,15 @@
-var level = require('level');
-var db = level('./db');
-var IFCBuilder = require('infinitechain_nodejs');
 var axios = require('axios');
-let env = require('./env');
 let fs = require('fs');
 
-var ifc = new IFCBuilder().setNodeUrl(env.nodeUrl).
-                           setWeb3Url(env.web3Url).
-                           setSignerKey(env.signerKey).
-                           setCipherKey(env.cipherKey).
-                           setStorage('level', db).
-                           setClientAddress('0x49aabbbe9141fe7a80804bdf01473e250a3414cb').
-                           setServerAddress('0x5b9688b5719f608f1cb20fdc59626e717fbeaa9a').
-                           build();
-
-var pkClient = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApFTF001PG7etLof0Oi8L\nYll3NApmlEw3Zz9zaitDbSAOcDv6L0yfpK4Kgg7Be/llQeaHAWifNnrewFhzKeid\nUZ9rZVZwZoOmbo7OaGdq+z+7VJxmkiJtZ6mD9BWxtjEVFQ3VByqmUc5v/8o7BaKh\nFGaxGMgmH8mwlPFJTXsVRRMb7Mdxp7PC4vkdUH2jorqmOreWEOMIbhVjHQAcVXIs\nseO1r1c2/4ZQIU3ofZMl8o6t4ldJRb9Y7y1xB7J4Egd4ClKCFoM9nQD7oCCN2ADW\nqkTDO91W0jVvx8OfUTEJlM+JiHODIYV8Oq+6sQJUe7rr/t9W7mp0I5UnaOo9HzoO\ncQIDAQAB\n-----END PUBLIC KEY-----';
-var pkStakeholder = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApFTF001PG7etLof0Oi8L\nYll3NApmlEw3Zz9zaitDbSAOcDv6L0yfpK4Kgg7Be/llQeaHAWifNnrewFhzKeid\nUZ9rZVZwZoOmbo7OaGdq+z+7VJxmkiJtZ6mD9BWxtjEVFQ3VByqmUc5v/8o7BaKh\nFGaxGMgmH8mwlPFJTXsVRRMb7Mdxp7PC4vkdUH2jorqmOreWEOMIbhVjHQAcVXIs\nseO1r1c2/4ZQIU3ofZMl8o6t4ldJRb9Y7y1xB7J4Egd4ClKCFoM9nQD7oCCN2ADW\nqkTDO91W0jVvx8OfUTEJlM+JiHODIYV8Oq+6sQJUe7rr/t9W7mp0I5UnaOo9HzoO\ncQIDAQAB\n-----END PUBLIC KEY-----';
-
 let sendIndex = (quotes) => {
-    let count = 0
-
     let intervalID = setInterval(async () => {
-        count++;
         let index = mean(quotes);
         let weightedIndex = weightedMean(quotes);
-        console.log('Index: ' + index);
-        console.log('Weighted Index: ' + weightedIndex);
+        await axios.post('http://localhost:3001/quotes', { quotes: quotes, index: index, weightedIndex: weightedIndex });
         quotes = nextQuotes(quotes);
-        let data = { index: index, weightedIndex: weightedIndex, timestamp: (new Date()).toString(), pkClient: pkClient, pkStakeholder: pkStakeholder };
-        let res = await sendRawPayment(count, data);
-        if (res.data.ok) {
-            let payment = res.data.payment;
-            let result = await ifc.client.verifyPayment(payment);
-            if (result) {
-                ifc.client.savePayment(payment);
-            }
-        } else {
-            console.log(res.data.message);
-            console.log('start resending');
-            if (res.data.code == 1) {
-                let res = await sendRawPayment(count, data, ifc.sidechain.getLatestStageHeight() + 2);
-                if (res.data.ok) {
-                    console.log('resend success');
-                    let payment = res.data.payment;
-                    let result = await ifc.client.verifyPayment(payment);
-                    if (result) {
-                        ifc.client.savePayment(payment);
-                    }
-                }
-            }
-        }
-    }, 2000);
+    }, 1000);
 }
-let sendRawPayment = async (count, data, stageHeight = null) => {
-    var rawPayment = ifc.client.makeRawPayment(0, count, data, stageHeight);
-    await ifc.client.saveRawPayment(rawPayment);
-    let res = await axios.post('http://localhost:3001/send', { rawPayment: rawPayment });
-    return res;
-}
+
 // Manipulate the volume and price randomly
 let nextQuotes = quotes => {
     return quotes.map(quote => {
@@ -91,52 +43,9 @@ let weightedMean = (quotes) => {
     return Math.round(r * 1000) / 1000;
 }
 
-// Watch blockchain event
-let watchBlockchainEvent = () => {
-    if (ifc.sidechain.getIFCContract()) {
-        ifc.event.watchAddNewStage(async (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                try {
-                    console.log("Add new stage event")
-
-                    let stageHash = result.args._stageHash;
-                    let rootHash = result.args._rootHash;
-                    console.log('stageHash: ' + stageHash);
-                    console.log('rootHash: ' + rootHash);
-
-                    // Audit
-                    stageHash = stageHash.substring(2);
-                    let paymentHashes = await ifc.client.getAllPaymentHashes(stageHash);
-                    paymentHashes.forEach(async (hash) => {
-                        let res = await ifc.client.audit(hash);
-                        let rawPayment = await ifc.client.getRawPayment(hash);
-                        let metadata = '{ Time: ' + rawPayment.data.timestamp + ', Index: ' + rawPayment.data.index + ', WeightedIndex: ' + rawPayment.data.weightedIndex + ' }'
-                        console.log(metadata + ', audit result: ' + res);
-                    });
-                } catch (e) {
-                    console.log(e);
-                }
-            }
-        });
-
-        ifc.event.watchFinalize((err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Finalize event")
-            }
-        });
-    } else {
-        setTimeout(watchBlockchainEvent, 1000);
-    }
-}
-
 // Select company whose CICS level code equals 6
 // Read quotes from file
 let runClient = () => {
-    watchBlockchainEvent();
     fs.readFile('./data/company.csv', (err, content) => {
         let companies = content.toString().split('\r\n');
         companies.shift();
@@ -164,4 +73,4 @@ let runClient = () => {
     });
 }
 
-module.exports = runClient;
+runClient();
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ let bodyParser = require('body-parser');
 let cors = require('cors');
 let IFCBuilder = require('infinitechain_nodejs');
 let path = require('path');
-let runClient = require('./client');
+var level = require('level');
+var db = level('./db');
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,53 +17,110 @@ let ifc = new IFCBuilder().setNodeUrl(env.nodeUrl).
                            setWeb3Url(env.web3Url).
                            setSignerKey(env.signerKey).
                            setCipherKey(env.cipherKey).
+                           setStorage('level', db).
+                           setClientAddress('0x49aabbbe9141fe7a80804bdf01473e250a3414cb').
+                           setServerAddress('0x5b9688b5719f608f1cb20fdc59626e717fbeaa9a').
                            build();
 
-app.post('/send', async function (req, res) {
+app.post('/quotes', async function (req, res) {
     try {
-        let rawPayment = req.body.rawPayment;
+        // Get quotes and index from parameters
+        let quotes = req.body.quotes;
+        let index = req.body.index;
+        let weightedIndex = req.body.weightedIndex;
+
+        // Make rawPayment from quotes
+        var keys = ifc.crypto.keyInfo();
+        let data = { quotes: quotes, index: index, weightedIndex: weightedIndex, timestamp: (new Date()).toString(), pkClient: keys.rsaPublicKey, pkStakeholder: keys.rsaPublicKey };
+        var rawPayment = ifc.client.makeRawPayment(0, 0, data);
+        await ifc.client.saveRawPayment(rawPayment);
+
+        // Send rawPayment to Node
         let payment = ifc.server.signRawPayment(rawPayment);
         console.log('Sent payment: ' + payment.paymentHash);
         let result = await ifc.server.sendPayments([payment]);
+
         if (result.ok) {
-            res.send({ ok: true, payment: payment });
+            ifc.client.savePayment(payment);
         } else {
-            res.send(result);
+            console.log(result.message);
+            console.log('start resending');
+            if (result.code == 1) {
+                var rawPayment = ifc.client.makeRawPayment(0, 0, data, ifc.sidechain.getLatestStageHeight() + 2);
+                await ifc.client.saveRawPayment(rawPayment);
+                let payment = ifc.server.signRawPayment(rawPayment);
+                console.log('Sent payment: ' + payment.paymentHash);
+                result = await ifc.server.sendPayments([payment]);
+
+                if (result.ok) {
+                    console.log('resend success');
+                    ifc.client.savePayment(payment);
+                }
+            }
         }
+
+        res.send(result);
     } catch (e) {
         console.log(e);
         res.status(500).send({ errors: e.message });
     }
 });
 
-app.post('/commit', async function (req, res) {
-    try {
-        let data = req.body.data;
-        let txHash = await ifc.server.commitPayments(10, 0, data);
-        console.log('Committed txHash: ' + txHash);
-        res.send({ ok: true, txHash: txHash });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({ errors: e.message });
-    }
-});
+let watchBlockchainEvent = () => {
+    if (ifc.sidechain.getIFCContract()) {
+        ifc.event.watchAddNewStage(async (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                try {
+                    console.log("Add new stage event")
+                    let stageHash = result.args._stageHash;
+                    let rootHash = result.args._rootHash;
+                    console.log('stageHash: ' + stageHash);
+                    console.log('rootHash: ' + rootHash);
 
-app.post('/finalize', async function (req, res) {
-    try {
-        let stageHeight = req.body.stageHeight;
-        let txHash = await ifc.server.finalize(stageHeight);
+                    // Audit
+                    stageHash = stageHash.substring(2);
+                    let paymentHashes = await ifc.client.getAllPaymentHashes(stageHash);
+                    paymentHashes.forEach(async (hash) => {
+                        let res = await ifc.client.audit(hash);
+                        let rawPayment = await ifc.client.getRawPayment(hash);
+                        let metadata = '{ Time: ' + rawPayment.data.timestamp + ', Index: ' + rawPayment.data.index + ', WeightedIndex: ' + rawPayment.data.weightedIndex + ' }'
+                        console.log(metadata + ', audit result: ' + res);
+                    });
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        });
 
-        res.send({ ok: true, txHash: txHash });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({ errors: e.message });
+        ifc.event.watchFinalize((err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Finalize event")
+            }
+        });
+    } else {
+        setTimeout(watchBlockchainEvent, 1000);
     }
-});
+}
 
 server.listen(3001, async function () {
     try {
         console.log('App listening on port 3001!');
-        runClient();
+        // Watch blockchain event
+        watchBlockchainEvent();
+
+        // Commmit Stage every 3 seconds
+        setInterval(async () => {
+            try {
+                let txHash = await ifc.server.commitPayments(86400, 0);
+                console.log('Committed txHash: ' + txHash);
+            } catch (e) {
+                console.log(e);
+            }
+        }, 3000);
     } catch (e) {
         console.error(e.message);
     }
