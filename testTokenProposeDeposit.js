bb33db7146b639aee810d0819e602c9c1579d850
sc0Vu <alk03073135@gmail.com>
2018-06-19 15:20:36 +0800

    Add testTokenProposeDeposit.js

---
+++ testTokenProposeDeposit.js
@@ -0,0 +1,38 @@
+let wizard = require('wizard_nodejs');
+let level = require('level');
+let env = require('./env');
+let axios = require('axios');
+let Web3 = require('web3');
+
+let db = level('./db');
+let InfinitechainBuilder = wizard.InfinitechainBuilder;
+let Receipt = wizard.Receipt;
+// let Types = wizard.Types;
+let url = 'http://127.0.0.1:3001/pay';
+let web3 = new Web3(new Web3.providers.HttpProvider(env.web3Url));
+
+let infinitechain = new InfinitechainBuilder()
+  .setNodeUrl(env.nodeUrl)
+  .setWeb3Url(env.web3Url)
+  .setSignerKey(env.signerKey)
+  .setStorage('level', db)
+  .build();
+
+infinitechain.initialize().then(async () => {
+  console.log('token proposeDeposit, you should transfer token to sidechain');
+
+  // onDeposit
+  infinitechain.event.onDeposit((err, result) => {
+    console.log('Deposit:');
+    console.log(result);
+  });
+
+  // proposeDeposit
+  let depositLightTx = await infinitechain.client.makeProposeDeposit();
+
+  let response = await axios.post(url, depositLightTx.toJson());
+  let depositReceiptJson = response.data;
+
+  let depositReceipt = new Receipt(depositReceiptJson);
+  await infinitechain.client.saveReceipt(depositReceipt);
+});
