e815c2a78cbb4a38319d14f6a5f04736029fd2d5
sc0Vu <alk03073135@gmail.com>
2018-06-19 17:54:47 +0800

    Fix testTokenProposeDeposit.js

--- testTokenProposeDeposit.js
+++ testTokenProposeDeposit.js
@@ -9,6 +9,7 @@ let InfinitechainBuilder = wizard.InfinitechainBuilder;
 let Receipt = wizard.Receipt;
 // let Types = wizard.Types;
 let url = 'http://127.0.0.1:3001/pay';
+let assetAddress = 'dee3a5a5603023db237ecc837d15803a5a62b088';
 
 let infinitechain = new InfinitechainBuilder()
   .setNodeUrl(env.nodeUrl)
@@ -26,12 +27,25 @@ infinitechain.initialize().then(async () => {
     console.log(result);
   });
 
-  // proposeDeposit
-  let depositLightTx = await infinitechain.client.makeProposeDeposit();
+  infinitechain.event.onProposeDeposit(async (err, result) => {
+    if (!err) {
+        let logID = result.args._dsn;
+        let nonce = infinitechain.client._getNonce();
+        let value = result.args._value;
+        let lightTxData = {
+            assetID: '0x' + assetAddress.padStart(64, '0'),
+            value: value,
+            fee: 0.01,
+            nonce: nonce,
+            logID: logID
+        };
 
-  let response = await axios.post(url, depositLightTx.toJson());
-  let depositReceiptJson = response.data;
+        let depositLightTx = await infinitechain.client.makeLightTx(0, lightTxData);
+        let response = await axios.post(url, depositLightTx.toJson());
+        let depositReceiptJson = response.data;
 
-  let depositReceipt = new Receipt(depositReceiptJson);
-  await infinitechain.client.saveReceipt(depositReceipt);
+        let depositReceipt = new Receipt(depositReceiptJson);
+        await infinitechain.client.saveReceipt(depositReceipt);
+    }
+  })
 });
